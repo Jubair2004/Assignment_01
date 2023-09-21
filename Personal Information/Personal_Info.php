af6ec0c18324b56c08708bd89a6419b4b85113cd
Jubair <mahmudjubair56@gmail.com>
2023-09-21 20:29:38 +0600

    Assingnment 01 done

---
+++ Personal Information/Personal_Info.php
@@ -0,0 +1,13 @@
+<?php
+
+//Task 1: Personal Information Page
+
+// Create a PHP file named personal_info.php that displays your personal information using variables and the echo statement. Include your name, age, country, and a brief introduction.
+
+
+$name = "Jubair Mahmud";
+$age = 18;
+$country = "Bangladesh";
+$description = "I'm a student.I love coding. Along with studying I am learning web development. Currently I am in the second batch of Ostad's PhP Laravel course.";
+
+echo "My name is $name. I'm $age year's old. I live in $country.$description ";
